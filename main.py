1ca4b0cac44c9f8cd260a49b68c2bad698675a16
McSpace <andrew.vilchinsky@gmail.com>
2025-01-06 16:39:08 +0100

    fix no lang in 11labs

--- main.py
+++ main.py
@@ -243,7 +243,7 @@ async def entrypoint(ctx: JobContext):
             model_id="eleven_multilingual_v2",
             voice=voice,
             api_key=os.getenv("ELEVENLABS_API_KEY"),
-            language=game_data.user_lang
+            # language=game_data.user_lang
         ),
         chat_ctx=initial_ctx,
         before_tts_cb=before_tts,
@@ -295,11 +295,11 @@ async def entrypoint(ctx: JobContext):
 
     async def finish_queue():
         logger.info("====== finish_queue =====")
-        await api_queue.join()
-        try:
-            await api_task
-        except asyncio.CancelledError:
-            pass
+        # await api_queue.join()
+        # try:
+        #     await api_task
+        # except asyncio.CancelledError:
+        #     pass
 
     async def on_session_end():
         logger.info("====== on_session_end. time to generate Preview =====")
