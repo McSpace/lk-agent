b521ee0c7cf874add1b436ee1bc2b86ebc7aca1d
McSpace <andrew.vilchinsky@gmail.com>
2025-01-09 15:27:25 +0100

    test before_llm return False

--- main.py
+++ main.py
@@ -143,6 +143,11 @@ async def entrypoint(ctx: JobContext):
 
     async def before_llm(assistant: VoicePipelineAgent, chat_context: str | AsyncIterable[str]):
         logger.info("====== before_LLM =====")
+        if isinstance(chat_context, str):
+            if str(chat_context).lower()== "отмена":
+                logger.info("User cancelled the game")
+                return False
+                
         current_user_text = chat_context.messages[-1].content if len(chat_context.messages) > 0 else None
         logger.info(current_user_text)
 
