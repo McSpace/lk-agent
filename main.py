7d38148f8a71e8fcd47034616afece7c857ca59f
McSpace <andrew.vilchinsky@gmail.com>
2025-01-09 16:04:13 +0100

    еуые

--- main.py
+++ main.py
@@ -143,18 +143,25 @@ async def entrypoint(ctx: JobContext):
 
     async def before_llm(assistant: VoicePipelineAgent, chat_context: str | AsyncIterable[str]):
         logger.info(f"====== before_LLM ==={chat_context}==")
-        if isinstance(chat_context, str):
-            if str(chat_context).lower() == "хорошо":
-                logger.info("User cancelled chat")
-                return False
-            else:
-                logger.info("User did not cancel chat")
-                return chat_context
-        else:
-            logger.info("chat_context is not a string")
+        # if isinstance(chat_context, str):
+        #     if str(chat_context).lower() == "хорошо":
+        #         logger.info("User cancelled chat")
+        #         return False
+        #     else:
+        #         logger.info("User did not cancel chat")
+        #         return chat_context
+        # else:
+        #     logger.info("chat_context is not a string")
             
 
         current_user_text = chat_context.messages[-1].content if len(chat_context.messages) > 0 else None
+        if current_user_text.lower() == "хорошо":
+            logger.info("User cancelled chat")
+            return False
+        else:
+            logger.info("User did not cancel chat")
+            return chat_context
+
         logger.info(current_user_text)
 
     async def before_tts(assistant: VoicePipelineAgent, text: str | AsyncIterable[str]):
