534abed483faaf87dc1163e73ffc24addc3768c3
McSpace <andrew.vilchinsky@gmail.com>
2025-01-09 16:10:06 +0100

    еьз

--- main.py
+++ main.py
@@ -155,12 +155,12 @@ async def entrypoint(ctx: JobContext):
             
 
         current_user_text = chat_context.messages[-1].content if len(chat_context.messages) > 0 else None
+        logger.info(current_user_text)
         if current_user_text.lower() == "хорошо":
             logger.info("User cancelled chat")
             return False
         else:
             logger.info("User did not cancel chat")
-            return chat_context
 
         logger.info(current_user_text)
 
