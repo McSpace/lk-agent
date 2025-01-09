bcf11f4ab8280465a0ae34946de5a6f25fa2dcd9
McSpace <andrew.vilchinsky@gmail.com>
2025-01-09 18:42:59 +0100

    чек

--- main.py
+++ main.py
@@ -149,15 +149,15 @@ async def entrypoint(ctx: JobContext):
         prev_user_text = ctx.proc.userdata.get("prev_user_text")
         if prev_user_text and current_user_text.startswith(prev_user_text):
             logger.info("Removing previous user text from current user text")
-            current_user_text = current_user_text[len(prev_user_text):]  
+            ctx.proc.userdata["prev_user_text"] = current_user_text[len(prev_user_text):]  
         
         logger.info(current_user_text)
-        if not current_user_text.lower().startswith("эй"):
+        if not current_user_text.lstrip().lower().startswith("эй"):
             logger.info("User cancelled chat")
 
             ctx.proc.userdata["prev_user_text"] = current_user_text
 
-            logger.info(f"new last message is {chat_context.messages}")
+            # logger.info(f"new last message is {chat_context.messages}")
             return False
         else:
             ctx.proc.userdata["prev_user_text"] = None
