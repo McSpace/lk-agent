efced0d4a7f1ad03c8cb19317383c69f5689ec4f
McSpace <andrew.vilchinsky@gmail.com>
2025-01-09 19:01:03 +0100

    fff

--- main.py
+++ main.py
@@ -147,6 +147,7 @@ async def entrypoint(ctx: JobContext):
 
         current_user_text = chat_context.messages[-1].content if len(chat_context.messages) > 0 else None
         prev_user_text = ctx.proc.userdata.get("prev_user_text")
+        logger.info(f"load prev_user_text: {prev_user_text}")
         if prev_user_text and current_user_text.startswith(prev_user_text):
             logger.info("Removing previous user text from current user text")
             current_user_text = current_user_text[len(prev_user_text):]  
@@ -162,6 +163,7 @@ async def entrypoint(ctx: JobContext):
         else:
             logger.info("No key - cancel chat")
             ctx.proc.userdata["prev_user_text"] = current_user_text
+            logger.info(f"save prev_user_text: {prev_user_text}")
 
             # logger.info(f"new last message is {chat_context.messages}")
             return False            
