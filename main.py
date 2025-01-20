48ec9e322424bbbd74719f5ef0e9283df6ea17be
McSpace <andrew.vilchinsky@gmail.com>
2025-01-20 11:39:36 +0100

    debud

--- main.py
+++ main.py
@@ -240,11 +240,15 @@ async def entrypoint(ctx: JobContext):
     #             ),
     #         api_key=os.getenv("ELEVENLABS_API_KEY"),
     #     )
+    
+    
     cartesia_voice = "da05e96d-ca10-4220-9042-d8acef654fa9" # default English voice
+    logger.info(f"User language: {game_data.user_lang}")
     if game_data.user_lang == "ru":
             cartesia_voice = "da05e96d-ca10-4220-9042-d8acef654fa9"
     elif game_data.user_lang == "nl":
             cartesia_voice = "9e8db62d-056f-47f3-b3b6-1b05767f9176"
+    logger.info(f"Cartesia voice: {cartesia_voice}")
          
     tts = cartesia.TTS(
         speed = 0.5,
