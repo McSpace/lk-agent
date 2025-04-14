c8b944718545578ba2364290c2c663f90cf58c04
McSpace <andrew.vilchinsky@gmail.com>
2025-04-14 19:41:50 +0200

    fix

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import dotenv
 
 from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, JobProcess, cli, llm
 from livekit.agents.pipeline import VoicePipelineAgent
-from livekit.agents.voice_assistant import VoiceAssistant
+# from livekit.agents.voice_assistant import VoiceAssistant
+from livekit.agents import VoiceAssistant
 # from livekit.agents import VoicePipelineAgent, vad
 from livekit.plugins import deepgram, openai, silero , elevenlabs, cartesia
 from livekit.plugins import turn_detector
